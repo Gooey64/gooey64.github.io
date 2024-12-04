ad2189ea346ab3fa69b7306fe38a00c15455dfc3
Lucie Bao <62731735+lucien-bao@users.noreply.github.com>
2024-12-04 17:03:05 -0500

    php code to query database for history and rankings; using cookies for username persistence across pages

---
+++ TypingTunes/rankings.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+    <title>Typing Tunes</title>
+</head>
+<body>
+    <header>
+        <div class="history">
+          <a href="history.html">History</a>
+        </div>
+        <h1>Typing Tunes</h1>
+        <div class="rankings">
+          <a href="rankings.html">Rankings</a>
+        </div>
+    </header>
+
+    <main>
+        <section class="song-list">
+        
+          <h1>Rankings</h1>
+
+          <?php
+            include "connect_db.php";
+
+            $sql = "SELECT * FROM games ORDER BY wpm DESC";
+            $result = $conn->query($sql);
+
+            echo "<table>";
+
+            // Header row
+            echo "<tr>";
+            echo "<th>Ranking</th>";
+            echo "<th>Username</th>";
+            echo "<th>Song</th>";
+            echo "<th>Artist</th>";
+            echo "<th>WPM</th>";
+            echo "<th>Accuracy</th>";
+            echo "</tr>";
+
+            $current_rank = 1;
+
+            // Build the table
+            while($row = $result->fetch_assoc()) {
+              echo "<tr>";
+              echo "<td>" . $current_rank . "</td>";
+              echo "<td>" . $row["username"] . "</td>";
+              echo "<td>" . $row["title"] . "</td>";
+              echo "<td>" . $row["artist"] . "</td>";
+              echo "<td>" . $row["wpm"] . "</td>";
+              echo "<td>" . $row["accuracy"] . "</td>";
+              echo "</tr>";
+
+              $current_rank++;
+            }
+
+            echo "</table>";
+          ?>
+        </section>
+    </main>
+
+    <footer>
+      <div class="history">
+        <a href="#">History</a>
+      </div>
+      <div>Typing Tunes</div>
+      <div class="rankings">
+        <a href="#">Rankings</a>
+      </div>
+    </footer>
+
+</body>
+</html>
