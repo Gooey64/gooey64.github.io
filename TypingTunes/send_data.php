f944e2a438769739bf05d697b69299cdf642eccb
Gooey64 <61029550+Gooey64@users.noreply.github.com>
2024-12-09 17:19:10 -0500

    Fixed the sql insertion query

--- TypingTunes/send_data.php
+++ TypingTunes/send_data.php
@@ -12,14 +12,16 @@
 
     $conn->select_db($db);
 
+    //creates php variables for the javascript variables being sent to database
     $user = $_POST['username'];
     $title = $_POST['title'];
     $artist = $_POST['artist'];
     $wpm = $_POST['wpm'];
     $accuracy = $_POST['accuracy'];
 
+    //sql query to insert user data to database
     $sql = "INSERT INTO games (username, title, artist, wpm, accuracy)
-        VALUES ('$user', '$title', '$artist', $wpm, $accuracy)";
+        VALUES ('$user', '$title', '$artist', '$wpm', '$accuracy')";
     
     if ($conn->query($sql) === TRUE) {
         echo json_encode(["message" => "Data inserted successfully"]);}
